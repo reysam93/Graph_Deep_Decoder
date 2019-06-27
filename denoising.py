2476c7a62d2fbeeeb95655e592ac296ebb2cbb96
Samuel Rey <samuel.rey.escudero@gmail.com>
2019-06-27 10:25:56 +0200

    optimiseng experiments

---
+++ denoising.py
@@ -0,0 +1,112 @@
+import sys
+import os
+import time, datetime
+from multiprocessing import Pool 
+sys.path.insert(0, 'graph_deep_decoder')
+from graph_deep_decoder import utils
+from graph_deep_decoder.architecture import GraphDeepDecoder
+from pygsp.graphs import StochasticBlockModel
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn as nn
+
+# Constants
+N_SIGNALS = 100 
+SEED = 15
+N_P = [0, .001, .005, .01, .05, .1, .5]
+EXPERIMENTS = [{'ups': 'original', 'arch': [2,2,2], 't': [4,16,64,None], 'gamma': None},
+               {'ups': 'no_A', 'arch': [2,2,2], 't': [4,16,64,None], 'gamma': None},
+               {'ups': 'weighted', 'arch': [2,2,2], 't': [4,16,64,None], 'gamma': 0},
+               {'ups': 'weighted', 'arch': [2,2,2], 't': [4,16,64,None], 'gamma': .75},
+               {'ups': 'original', 'arch': [6,6,6], 't': [4,16,64,None], 'gamma': None},
+               {'ups': 'no_A', 'arch': [6,6,6], 't': [4,16,64,None], 'gamma': None},
+               {'ups': 'weighted', 'arch': [6,6,6], 't': [4,16,64,None], 'gamma': 0},
+               {'ups': 'weighted', 'arch': [6,6,6], 't': [4,16,64,None], 'gamma': .75},
+               {'ups': 'original', 'arch': [4,4,4,4], 't': [4,16,64,128,None], 'gamma': None},
+               {'ups': 'no_A', 'arch': [4,4,4,4], 't': [4,16,64,128,None], 'gamma': None},
+               {'ups': 'weighted', 'arch': [4,4,4,4], 't': [4,16,64,128,None], 'gamma': 0},
+               {'ups': 'weighted', 'arch': [4,4,4,4], 't': [4,16,64,128,None], 'gamma': .75},]
+
+G_PARAMS = [{'type': 'SBM','N': 256,'k': 4,'p': 0.15,'q': 0.01/4},
+            {'type': 'SBM','N': 512,'k': 4,'p': 0.075,'q': 0.001},
+            {'type': 'SBM','N': 1024,'k': 10,'p': 0.1,'q': 0.0005}]
+
+def compute_clusters(G, root_clust):
+    sizes = []
+    descendances = []
+    hier_As = []
+    for exp in EXPERIMENTS:
+        exp['t'][-1] = G.N
+        cluster = utils.MultiRessGraphClustering(G, exp['t'], root_clust)
+        sizes.append(cluster.clusters_size)
+        descendances.append(cluster.compute_hierarchy_descendance())
+        hier_As.append(cluster.compute_hierarchy_A(exp['ups']))
+    return sizes, descendances, hier_As
+
+def denoise(x, sizes, descendances, hier_As, n_p):
+    mse_est = np.zeros(len(EXPERIMENTS))
+    params = np.zeros(len(EXPERIMENTS))
+    x_n = x + np.random.randn(x.size)*np.sqrt(n_p)
+    for i, exp in enumerate(EXPERIMENTS):
+        dec = GraphDeepDecoder(descendances[i], hier_As[i], sizes[i],
+                        n_channels=exp['arch'], upsampling=exp['ups'],
+                        gamma=exp['gamma'])
+        dec.build_network()
+        x_est, _ = dec.fit(x_n)
+        mse_est[i] = np.mean(np.square(x-x_est))/np.linalg.norm(x)
+        params[i] = dec.count_params()
+    return mse_est, params
+
+def print_results(N, mean_mse, params, n_p):
+    print('NOISE POWER:', n_p)
+    for i, exp in enumerate(EXPERIMENTS):
+        print('{}. (EXP {}) '.format(i, exp))
+        print('\tMean MSE: {}\tParams: {}\tCompression: {}'
+                            .format(mean_mse[i], params[i], N/params[i]))
+
+def plot_results(mean_mse):
+    plt.figure()
+    plt.plot(N_P, mean_mse)
+    plt.xlabel('Noise Power')
+    plt.ylabel('Mean MSE')
+    legend = [str(exp) for exp in EXPERIMENTS]
+    plt.legend(legend)
+
+def save_results(mse_est, N):
+    if not os.path.isdir('./results'):
+        os.makedirs('./results')
+    timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M")
+    np.save('./results/denoise_N_{}_{}'.format(N, timestamp), mse_est)
+
+
+if __name__ == '__main__':
+    L = 5
+
+    # Set seeds
+    np.random.seed(SEED)
+    GraphDeepDecoder.set_seed(SEED)
+
+    start_time = time.time()
+    for g_params in G_PARAMS:
+        G = utils.create_graph(g_params, seed=SEED)
+        sizes, descendances, hier_As = compute_clusters(G, g_params['k'])
+        print('Graph', g_params)
+
+        mse_est = np.zeros((len(N_P), N_SIGNALS, len(EXPERIMENTS)))
+        for i, n_p in enumerate(N_P):    
+            with Pool() as pool:
+                for j in range(N_SIGNALS):
+                    signal = utils.DifussedSparseGraphSignal(G,L,g_params['k'])
+                    signal.to_unit_norm()
+                    result = pool.apply_async(denoise,
+                                args=[signal.x, sizes, descendances, hier_As, n_p])
+                for j in range(N_SIGNALS):
+                    mse_est[i,j,:], n_params = result.get()
+
+            print_results(g_params['N'], np.mean(mse_est[i,:,:], axis=0), n_params, n_p)
+        plot_results(np.mean(mse_est, axis=1))
+        save_results(mse_est, g_params['N'])
+
+    print('--- {} minutes ---'.format((time.time()-start_time)/60))
+    plt.show()
---
+++ test_graph.py
@@ -0,0 +1,119 @@
+"""
+Check the efect of the graph
+"""
+
+import sys
+import time
+from multiprocessing import Pool 
+sys.path.insert(0, 'graph_deep_decoder')
+from graph_deep_decoder import utils
+from graph_deep_decoder.architecture import GraphDeepDecoder
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+
+
+# Tuning parameters
+n_signals = 50
+L = 5
+n_p = 0.1 # SNR = 1/n_p
+batch_norm = True #True
+up_method = 'weighted'
+t = [4, 16, 64, None]
+c_method = 'maxclust' # 'maxclust' or 'distance'
+alg = 'spectral_clutering'
+link = 'average'
+n_chans = [2,2,2]
+last_act_fun = nn.Tanh()
+gamma = 0.5
+
+# Constants
+SEED = 15
+G_PARAMS = [{'type': 'SBM','N': 256,'k': 2,'p': 0.1,'q': 0.01/4},
+          {'type': 'SBM','N': 256,'k': 4,'p': 0.15,'q': 0.01/4},
+          {'type': 'SBM','N': 256,'k': 10,'p': 0.4,'q': 0.01/4},
+          {'type': 'SBM','N': 512,'k': 2,'p': 0.05,'q': 0.001},
+          {'type': 'SBM','N': 512,'k': 4,'p': 0.075,'q': 0.001},
+          {'type': 'SBM','N': 512,'k': 10,'p': 0.15,'q': 0.001},
+          {'type': 'SBM','N': 1024,'k': 2,'p': 0.05,'q': 0.00075},
+          {'type': 'SBM','N': 1024,'k': 4,'p': 0.075,'q': 0.0005},
+          {'type': 'SBM','N': 1024,'k': 10,'p': 0.1,'q': 0.0005},
+          {'type': 'ER','N': 256,'k': 4,'p': 0.05},
+          {'type': 'ER','N': 512,'k': 4,'p': 0.025},
+          {'type': 'ER','N': 1024,'k': 4,'p': 0.01}]
+ 
+N_SCENARIOS = len(G_PARAMS)
+
+def compute_clusters(Gs):
+    sizes = []
+    descendances = []
+    hier_As = []
+
+    for i, G in enumerate(Gs):
+        t[-1] = G.N
+        cluster = utils.MultiRessGraphClustering(G, t, G_PARAMS[i]['k'], alg,
+                                                    method=c_method, link_fun=link)
+        descendances.append(cluster.compute_hierarchy_descendance())
+        hier_As.append(cluster.compute_hierarchy_A(up_method))
+        sizes.append(cluster.clusters_size)
+    return sizes, descendances, hier_As
+
+def compute_signals(Gs):
+    signals = []
+    for i, G in enumerate(Gs):
+        signal = utils.DifussedSparseGraphSignal(G,L,G_PARAMS[i]['k'],-1,1)
+        signal.to_unit_norm()
+        signals.append(signal.x)
+    return signals
+
+def test_graphs(signals, sizes, descendances, hier_As):
+    mse_est = np.zeros(N_SCENARIOS)
+    mse_fit = np.zeros(N_SCENARIOS)
+    for i in range(N_SCENARIOS):
+        x = signals[i]
+        x_n = x + np.random.randn(x.size)*np.sqrt(n_p)
+
+        dec = GraphDeepDecoder(descendances[i], hier_As[i], sizes[i],
+                        n_channels=n_chans, upsampling=up_method, batch_norm=batch_norm,
+                        last_act_fun=last_act_fun, gamma=gamma)
+        dec.build_network()
+        x_est, mse_fit[i] = dec.fit(x_n)
+        mse_est[i] = np.mean(np.square(x-x_est))/np.linalg.norm(x)
+    mse_fit = mse_fit/np.linalg.norm(x_n)
+    return mse_est, mse_fit
+
+def print_results(mean_mse, mean_mse_fit, clust_sizes):
+    for i in range(N_SCENARIOS):
+        print('{}. (GRAPH: {}) '.format(i, G_PARAMS[i]))
+        print('\tMean MSE: {}\tClust Sizes: {}\tMSE fit {}'
+                            .format(mean_mse[i], clust_sizes[i], mean_mse_fit[i]))
+
+
+if __name__ == '__main__':
+    input_size = 4
+
+    # Set seeds
+    np.random.seed(SEED)
+    GraphDeepDecoder.set_seed(SEED)
+
+    Gs = [utils.create_graph(G_PARAMS[i], seed=SEED) for i in range(N_SCENARIOS)] 
+    sizes, descendances, hier_As = compute_clusters(Gs)
+    
+    start_time = time.time()
+    mse_fit = np.zeros((n_signals, N_SCENARIOS))
+    mse_est = np.zeros((n_signals, N_SCENARIOS))
+    with Pool() as pool:
+        for i in range(n_signals):
+            signals = compute_signals(Gs)
+            result = pool.apply_async(test_graphs, 
+                                        args=[signals, sizes, descendances, hier_As])
+
+        for i in range(n_signals):
+            mse_est[i,:], mse_fit[i,:] = result.get()
+
+    # Print result:
+    print('--- {} minutes ---'.format((time.time()-start_time)/60))
+    print_results(np.mean(mse_est, axis=0), np.mean(mse_fit, axis=0), sizes)
+
