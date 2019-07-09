52568f2d8569a41a98dad2aab7dd9990ec4c54ce
Samuel Rey <samuel.rey.escudero@gmail.com>
2019-07-09 17:46:37 +0200

    preprin exp for paper

--- denoising_real_data.py
+++ denoising_real_data.py
@@ -11,22 +11,13 @@ from pygsp.graphs import Graph
 
 SEED = 15
 DATASET_PATH = 'dataset/graphs.mat'
-MAX_SIGNALS = 2
+MAX_SIGNALS = 100
 MIN_SIZE = 75
-ATTR = 6
-N_P = [0, .1, .3, .5]
-EXPERIMENTS = [{'ups': 'original', 'arch': [3,3,3], 't': [4,16,64,None], 'gamma': None},
-               {'ups': 'no_A', 'arch': [3,3,3], 't': [4,16,64,None], 'gamma': None},
-               {'ups': 'binary', 'arch': [3,3,3], 't': [4,16,32,None], 'gamma': 0},
-               {'ups': 'binary', 'arch': [3,3,3], 't': [4,16,32,None], 'gamma': 0.5},
-               {'ups': 'weighted', 'arch': [3,3,3], 't': [4,16,32,None], 'gamma': 0},
-               {'ups': 'weighted', 'arch': [3,3,3], 't': [4,16,32,None], 'gamma': 0.5},
-               {'ups': 'original', 'arch': [4,4,4,4], 't': [4,16,32,64,None], 'gamma': None},
-               {'ups': 'no_A', 'arch': [4,4,4,4], 't': [4,16,32,64,None], 'gamma': None},
-               {'ups': 'binary', 'arch': [4,4,4,4], 't': [4,8,16,32,None], 'gamma': 0},
-               {'ups': 'binary', 'arch': [4,4,4,4], 't': [4,8,16,32,None], 'gamma': 0.5},
-               {'ups': 'weighted', 'arch': [4,4,4,4], 't': [4,8,16,32,None], 'gamma': 0},
-               {'ups': 'weighted', 'arch': [4,4,4,4], 't': [4,8,16,32,None], 'gamma': 0.5}]
+ATTR = 7
+N_P = [0, .1, .2, .3, .4, .5, .6, .7]
+EXPERIMENTS = ['bandlimited',
+               {'ups': 'original', 'arch': [3,3,3], 't': [4,16,32,None], 'gamma': None},
+               {'ups': 'weighted', 'arch': [3,3,3], 't': [4,16,32,None], 'gamma': .5}]
 
 """
 EXPERIMENTS = [{'ups': 'original', 'arch': [3,3,3], 't': [4,16,64,None], 'gamma': None},
@@ -44,8 +35,6 @@ EXPERIMENTS = [{'ups': 'original', 'arch': [3,3,3], 't': [4,16,64,None], 'gamma'
 """
 N_EXPS = len(EXPERIMENTS)
 
-FMTS = ['o-', '^-', '+-', 'x-', 'o--', '^--', '+--', 'x--', 'o:', '^:', '+:', 'x:']
-
 
 def read_graphs():
     signals = []
@@ -61,6 +50,7 @@ def read_graphs():
         signal = utils.DeterministicGS(G, graphs_mat['cell_X'][i][0][:,ATTR])
         signal.signal_to_0_1_interval()
         signal.to_unit_norm()
+        G.compute_fourier_basis()
         Gs.append(G)
         signals.append(signal)
         
@@ -77,6 +67,12 @@ def compute_clusters(Gs):
         descendances.append([])
         hier_As.append([])
         for exp in EXPERIMENTS:
+            if not isinstance(exp,dict):
+                sizes[i].append(None)
+                descendances[i].append(None)
+                hier_As[i].append(None)
+                continue
+
             exp['t'][-1] = G.N
             root_clust = exp['arch'][0]
             cluster = utils.MultiRessGraphClustering(G, exp['t'], root_clust)
@@ -85,48 +81,39 @@ def compute_clusters(Gs):
             hier_As[i].append(cluster.compute_hierarchy_A(exp['ups']))
     return sizes, descendances, hier_As
 
-def denoise_real(id, x, sizes, descendances, hier_As, n_p):
+def denoise_real(id, x, sizes, descendances, hier_As, n_p, V):
     error = np.zeros(N_EXPS)
-    params = np.zeros(N_EXPS)
     x_n = utils.GraphSignal.add_noise(x, n_p)
     for i, exp in enumerate(EXPERIMENTS):
-        dec = GraphDeepDecoder(descendances[i], hier_As[i], sizes[i],
-                        n_channels=exp['arch'], upsampling=exp['ups'],
-                        gamma=exp['gamma'], last_act_fun=nn.Sigmoid())
-        dec.build_network()
-        x_est, _ = dec.fit(x_n, n_iter=4000)
+        if not isinstance(exp,dict):
+            x_est = utils.bandlimited_model(x_n, V)
+        else:
+            dec = GraphDeepDecoder(descendances[i], hier_As[i], sizes[i],
+                            n_channels=exp['arch'], upsampling=exp['ups'],
+                            gamma=exp['gamma'], last_act_fun=nn.Sigmoid())
+            dec.build_network()
+            x_est, _ = dec.fit(x_n, n_iter=4000)
         error[i] = np.sum(np.square(x-x_est))/np.square(np.linalg.norm(x))
-        params[i] = dec.count_params()
         print('Signal: {} Scenario {}: Error: {:.4f}'
                             .format(id, i, error[i]))
-    return error, params
+    return error
 
-def print_results(mse_est, params, n_p):
+def print_results(mse_est, n_p):
     mean_mse = np.mean(mse_est, axis=0)
     median_mse = np.median(mse_est, axis=0)
     std = np.std(mse_est, axis=0)
     print('NOISE POWER:', n_p)
     for i, exp in enumerate(EXPERIMENTS):
         print('{}. (EXP {}) '.format(i, exp))
-        print('\tMean Error: {}\tMedian Error: {}\tSTD: {}\tParams: {}'
-                    .format(mean_mse[i], median_mse[i], std[i], params[i]))
+        print('\tMean Error: {}\tMedian Error: {}\tSTD: {}'
+                    .format(mean_mse[i], median_mse[i], std[i]))
 
-def plot_results(mean_mse):
-    plt.figure()
-    for i in range(N_EXPS):
-        plt.plot(N_P, mean_mse[:,i], FMTS[i])
-    
-    plt.xlabel('Noise Power')
-    plt.ylabel('Mean MSE')
-    legend = [str(exp) for exp in EXPERIMENTS]
-    plt.legend(legend)
-
-def save_results(mse_est):
+def save_results(error):
     if not os.path.isdir('./results/denoising'):
         os.makedirs('./results/denoising')
     timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M")
-    data = {'mse': mse_est, 'EXPERIMENTS': EXPERIMENTS, 'N_P': N_P,
-            'SEED': SEED, 'FMTS': FMTS}
+    data = {'error': error, 'EXPERIMENTS': EXPERIMENTS, 'N_P': N_P,
+            'SEED': SEED}
     path = './results/denoising/denoise_real_' + timestamp
     np.save(path, data)
     print('SAVED as:', path)
@@ -140,21 +127,21 @@ if __name__ == '__main__':
     n_signals = len(signals)
     sizes, descendances, hier_As = compute_clusters(Gs)
 
-    mse_est = np.zeros((len(N_P), n_signals, N_EXPS))
+    error = np.zeros((len(N_P), n_signals, N_EXPS))
     start_time = time.time()
-    results = []
-    for i, n_p in enumerate(N_P):  
+    for i, n_p in enumerate(N_P):
+        print('noise',i,':',n_p)
+        results = []
         with Pool() as pool:
             for j in range(n_signals):
                 results.append(pool.apply_async(denoise_real,
                         args=[j, signals[j].x, sizes[j], descendances[j],
-                                hier_As[j], n_p]))
+                                hier_As[j], n_p, Gs[j].U]))
 
             for j in range(n_signals):
-                mse_est[i,j,:], n_params = results[j].get()
+                error[i,j,:] = results[j].get()
 
-        print_results(mse_est[i,:,:], n_params, n_p)
-    plot_results(np.mean(mse_est, axis=1))
-    save_results(mse_est)
+        print_results(error[i,:,:], n_p)
+    
+    save_results(error)
     print('--- {} minutes ---'.format((time.time()-start_time)/60))
-    plt.show()
